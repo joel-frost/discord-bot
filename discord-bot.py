bfb8f0f5aef2df849e409af8490b915b9362709c
Joel Frost <joelfrost4@gmail.com>
2021-03-26 17:39:31 +0000

    Added time and help commands

--- discord-bot.py
+++ discord-bot.py
@@ -19,7 +19,7 @@ async def on_ready():
 async def start_timer(ctx):
     work_em = discord.Embed(title="Time to start working", color=10181046)
     await ctx.send(embed = work_em)
-    
+
     timer.start()
     while timer.get_status():
         await asyncio.sleep(1)
@@ -35,5 +35,21 @@ async def stop_timer(ctx):
     stop_em = discord.Embed(title="Timer stopped", color=15158332)
     await ctx.send(embed = stop_em)
     timer.stop()
+
+@bot.command(name="time", help="Show remaining time")
+async def show_time(ctx):
+    await ctx.send(f"Current time is: {timer.get_ticks()}")
+    await ctx.send(f"Current timer status is: {timer.get_status()}")
+
+@bot.command(name="helpme", help="Show help text")
+async def show_help(ctx):
+    help_commands = dict()
+    for command in bot.commands:
+        help_commands[command.name] = command.help
+    description = "Bot commands are: {}".format(help_commands)
+
+
+    show_help_em = discord.Embed(title="A simple Pomodoro timer bot", description=description, color=15158332)
+    await ctx.send(embed = show_help_em)
 
 bot.run(os.environ['BOT_TOKEN'])
