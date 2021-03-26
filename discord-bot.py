f0b8e3335f4e9f1bf680d57950459a9a0951f3b2
Joel Frost <joelfrost4@gmail.com>
2021-03-26 15:41:51 +0000

    Changed to Bot API

--- discord-bot.py
+++ discord-bot.py
@@ -1,21 +1,19 @@
 import discord
 import os
 from dotenv import load_dotenv
+from discord.ext import commands
+
 
 load_dotenv()
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='!')
 
-@client.event
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {}'.format(bot.user))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@bot.command(name="start", help="Start a pomodoro timer")
+async def start_timer(ctx):
+    await ctx.send("Time to work!")
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-client.run(os.environ['BOT_TOKEN'])
+bot.run(os.environ['BOT_TOKEN'])
