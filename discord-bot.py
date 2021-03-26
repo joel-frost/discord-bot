a0d9e823a2ed3213464a08dfef1e7287bfbeb384
Joel Frost <joelfrost4@gmail.com>
2021-03-26 17:24:35 +0000

    Created Timer class and added stop command

--- discord-bot.py
+++ discord-bot.py
@@ -1,12 +1,15 @@
 import asyncio
 import discord
 import os
+from timer import Timer
 from dotenv import load_dotenv
 from discord.ext import commands
 
 load_dotenv()
 
 bot = commands.Bot(command_prefix='!')
+
+timer = Timer()
 
 @bot.event
 async def on_ready():
@@ -16,7 +19,14 @@ async def on_ready():
 async def start_timer(ctx):
     work_em = discord.Embed(title="Time to start working", color=10181046)
     await ctx.send(embed = work_em)
-    await asyncio.sleep(3)
+    
+    timer.start()
+    while timer.get_status():
+        await asyncio.sleep(1)
+        timer.tick()
+        if timer.get_ticks() >= timer.get_max_ticks():
+            timer.stop()
+
     break_em = discord.Embed(title="Time for a break", color=3066993)
     await ctx.send(embed=break_em)
 
@@ -24,5 +34,6 @@ async def start_timer(ctx):
 async def stop_timer(ctx):
     stop_em = discord.Embed(title="Timer stopped", color=15158332)
     await ctx.send(embed = stop_em)
+    timer.stop()
 
 bot.run(os.environ['BOT_TOKEN'])
